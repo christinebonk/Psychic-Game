c3fe677b296b6666d262e54c97c91eff9a46f9c1
Christine Bonk <christinebonk@gmail.com>
2018-04-15 17:58:48 -0400

    replays game on win

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,55 @@
 
 
-//Set max number of guess
+//Set Variables
 var guessNumber = 15; 
-console.log("Guess Number: " + guessNumber);
-document.getElementById("guess").innerHTML = "<h3>" + guessNumber + "</h3>";
-
-//Generate word
-var words = ["psycho", "halloween", "jaws", "seven", "alien", "scream"];
 var currentWord;
-
-function generateWord() {
-	currentWord = words[Math.floor(Math.random()*words.length)];
-}
-
-generateWord();
-
-console.log(currentWord);
-
-//Display current progress for words
-
-	//userProgress to define progress before guess
 var userProgress = "";
-
-	//userProgress begins as the same number of underscores as letters in the word
-for (i=0;i<currentWord.length;i++) {
-	userProgress = userProgress + ("_")
-	document.getElementById("word").innerHTML = "<h2>" + userProgress + "</h2>";
-}
-	//currentProgress to define progress after guess
 var currentProgress = "";
 var lettersGuessed = "";
 var repeat = false;
-console.log(userProgress);
-console.log(lettersGuessed);
+var winCount = 0;
 
+//Display Remaining Guesses
+function displayRemainingGuesses() {
+	document.getElementById("guess").innerHTML = "<h3>" + guessNumber + "</h3>";
+}
 
+//Generates New Word
+function generateWord() {
+	var words = ["psycho", "halloween", "jaws", "seven", "alien", "scream"];
+	currentWord = words[Math.floor(Math.random()*words.length)];
+}
+
+//Displays underscores in place of letters
+function displayProgress() {
+	for (i=0;i<currentWord.length;i++) {
+	userProgress = userProgress + ("_")
+	document.getElementById("word").innerHTML = "<h2>" + userProgress + "</h2>";
+	}
+}
+
+//Displays Wins
+function displayWins() {
+	document.getElementById("win-count").innerHTML = winCount;
+}
+
+//Play Game Function
+function playGame() {
+	//Reset variables
+	guessNumber = 15; 
+	userProgress = "";
+	currentProgress = "";
+	lettersGuessed = "";
+	repeat = false;
+	//Call game functions
+	displayRemainingGuesses();
+	generateWord();
+	displayProgress();
+	displayWins();
+}
+
+//Call Play Game when documented loaded
+playGame();
 
 //When a key is pressed
 document.onkeyup = function () {
@@ -121,8 +136,13 @@ document.onkeyup = function () {
 		var repeat = false; 
 		console.log(userProgress);
 
+
+		//checks for win
 		if (currentWord == userProgress) {
 			document.getElementById("win").innerHTML = "Hello";
+			winCount = winCount + 1;
+			document.getElementById("win-count").innerHTML = winCount;
+			playGame(); 
 		} else {
 
 		}
